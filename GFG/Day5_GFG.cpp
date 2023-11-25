16f93a57fbc7176243ee29cb19879231d05565e6
Keerthana-P-G <118243392+Keerthana-P-G@users.noreply.github.com>
2023-11-25 17:38:11 +0530

    Update Day5_GFG.cpp

--- GFG/Day5_GFG.cpp
+++ GFG/Day5_GFG.cpp
@@ -17,7 +17,9 @@ Output:
 1 4 2 5 3 6
 */
 
-void solve(int arr[], int i, int j, int n) {
+class Solution{
+	public:
+	void solve(int arr[], int i, int j, int n) {
 	    if(n <= 0)
 	        return;
 	        
@@ -29,3 +31,10 @@ void solve(int arr[], int i, int j, int n) {
 	   arr[n-2] = a;
 	   arr[n-1] = b;
 	}
+	void shuffleArray(int arr[],int n)
+	{
+	    int i=n/2-1;
+	    int j=n-1;
+	    solve(arr,i,j,n);
+	}
+};
